65b4bf75f52f2203725d19362dcee78fff7fc175
subbusubash24 <107912022+subbusubash24@users.noreply.github.com>
2022-09-09 02:00:56 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// intro=document.getElementById('intro');
+
+// h1=document.createElement('h1');
+// h1.innerText="Hello..!!!"
+// h1.id="demo"
+
+// intro.appendChild(h1);
+
+// intro.removeChild(h1);
+// console.log(h1);
+
+
+document.getElementById('intro').setAttribute('value' ,'watermelon');
